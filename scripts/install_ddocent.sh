9ed3906ac45293cca9667fda439c55aec0a136f8
Chris Friedline <chris@friedline.net>
2016-08-30 11:44:20 -0400

    sed cd-hit url

--- scripts/install_ddocent.sh
+++ scripts/install_ddocent.sh
@@ -6,4 +6,5 @@ rm -rf /dDocent_run
 mkdir -p /dDocent_run
 git clone https://github.com/jpuritz/dDocent.git dDocent_src
 cd dDocent_src
+sed -i s%https://cdhit.googlecode.com/files/cd-hit-v4.6.1-2012-08-27.tgz%https://github.com/weizhongli/cdhit/releases/download/V4.6.1/cd-hit-v4.6.1-2012-08-27.tgz% install_dDocent_requirements
 bash install_dDocent_requirements /dDocent_run
